870ccc5b1c54361233b735050f6e5d4fadfe4733
Nacho <nacho@FEIT4533.local>
2015-05-19 12:29:54 +1000

    Initial upload

---
+++ KirakiraVIEW/KirakiraVIEW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KirakiraVIEW
+//
+//  Created by Nacho on 19/05/2015.
+//  Copyright (c) 2015 Jack Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KirakiraVIEW/KirakiraVIEW/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  KirakiraVIEW
+//
+//  Created by Nacho on 19/05/2015.
+//  Copyright (c) 2015 Jack Cho. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+#define HAS_LIBCXX
+#import <Structure/Structure.h>
+
+@interface ViewController : UIViewController <STSensorControllerDelegate>
+
+@end
+
+
